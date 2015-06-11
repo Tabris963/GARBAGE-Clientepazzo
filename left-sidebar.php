8c29c809e9a1262dda601baf037b18d50c7e8654
Riccardo Zenere <riccardo.zenere94@gmail.com>
2015-06-11 16:43:49 +0200

    Modified structure of index/product-details
    Modified:
    footer.php
    index.php
    product-details.php
    
    Saved new store images (x4)

---
+++ left-sidebar.php
@@ -0,0 +1,24 @@
+  <div class="col-sm-3">
+    <div class="left-sidebar">
+      <h2>Categorie</h2>
+      <div class="panel-group category-products" id="accordian"><!--category-productsr-->
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title"><a href="#">Vestiti</a></h4>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title"><a href="#">Antiquariato</a></h4>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title"><a href="#">Prodotti biologici</a></h4>
+          </div>
+        </div>
+
+      </div><!--/category-products-->
+    </div>
+  </div>
