4b9c78b0280a05f55b13bde5d9c4d5a43d3ff15d
Riccardo Zenere <riccardo.zenere94@gmail.com>
2015-07-08 00:25:14 +0200

    Modified buttons (now 'Visualizza dettagli')

--- js/index.js
+++ js/index.js
@@ -25,10 +25,10 @@ function getProducts(){
         tmp += '<div class="product-image-wrapper"><div class="single-products"><div class="productinfo text-center">';
         tmp += '<img src="images/products/'+resp.products[i].id+'.jpg" alt="" />';
         tmp += '<h2>€'+resp.products[i].prezzo+'</h2><p>'+resp.products[i].nome+'</p>';
-        tmp += '<a href="product-details.php?id='+resp.products[i].id+'" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Aggiungi al carrello</a>';
+        tmp += '<a href="product-details.php?id='+resp.products[i].id+'" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Visualizza dettagli</a>';
         tmp += '</div><div class="product-overlay"><div class="overlay-content"><h2>€'+resp.products[i].prezzo+'</h2>';
         tmp += '<p>'+resp.products[i].nome+'</p>';
-        tmp += '<a href="product-details.php?id='+resp.products[i].id+'" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Aggiungi al carrello</a>';
+        tmp += '<a href="product-details.php?id='+resp.products[i].id+'" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Visualizza dettagli</a>';
         tmp += '</div></div></div></div></div>';
         $("#allProd").append(tmp);
       }
@@ -59,7 +59,7 @@ function getProducts(){
         tmp += '<img src="images/products/'+resp.products[i].id+'.jpg" alt="'+resp.products[i].descrizione+'" />';
         tmp += '<h2>€'+resp.products[i].prezzo+'</h2>';
         tmp += '<p>'+resp.products[i].nome+'</p>';
-        tmp += '<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Aggiungi al carrello</a></div></div></div></div>';
+        tmp += '<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Visualizza dettagli</a></div></div></div></div>';
         if(resp.products[i].id_categoria == 1){
           $("#prodottiBiologici").append(tmp);
         }else{
